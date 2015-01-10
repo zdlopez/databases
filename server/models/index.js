848c6c34ff53db5c3f684009928d6f36ab0c6a6f
zdlopez <zachary.d.lopez@gmail.com>
2015-01-10 10:22:22 -0800

    writing of the full message with foreign keys to db works.

--- server/models/index.js
+++ server/models/index.js
@@ -35,28 +35,32 @@ var postMessage = function (data) {
     console.log(rows);
   });
   //get userID
-  var idQuery ="select (uID) from users where userName='"+data.username+"'";
-  var uID;
-  db.connection.query(idQuery, function(err, rows){
-    uID = rows[0].uID;
-  });
+  // var idQuery ="select (uID) from users where userName='"+data.username+"'";
+  // var uID;
+  // db.connection.query(idQuery, function(err, rows){
+  //   uID = rows[0].uID;
+  // });
   //add room
   var roomQuery = "insert into rooms (roomName) select * from (select '"+ data.roomname +"') as tmp where not exists (select roomName from rooms where roomName = '"+ data.roomname +"') limit 1";
   db.connection.query(roomQuery, function(err, rows){
     console.log(rows);
   });
   //get roomID
-  var roomIDQuery = "select (rmID) from rooms where roomName='"+data.roomname+"'";
-  var roomID;
-  db.connection.query(roomIDQuery, function(err, rows){
-    console.log(rows);
-    roomID = rows[0].rmID;
-    console.log(roomID);
-  });
+  // var roomIDQuery = "select (rmID) from rooms where roomName='"+data.roomname+"'";
+  // var roomID;
+  // db.connection.query(roomIDQuery, function(err, rows){
+  //   console.log(rows);
+  //   roomID = rows[0].rmID;
+  //   console.log(roomID);
+  // });
   //add all it main
   data.createdAt = JSON.stringify(data.createdAt);
-  // var insertMSG = "insert into messages (createdAt, fk_uID, text, fk_rmID) values ('" + data.createdAt + "'," + parseInt(uID) + ", '" + data.text + "'," + parseInt(roomID) + ")";
-  var insertMSG = "insert into messages (createdAt, fk_uID) values ('" + data.createdAt + "',"+parseInt(uID)+")";
+  var insertMSG = "insert into messages (createdAt, fk_uID, text, fk_rmID) values ('" + data.createdAt + "',(select uID from users where userName ='" + data.username + "'), '" + data.text + "',(select rmID from rooms where roomName = '"+ data.roomname +"'))";
+  //var insertMSG = "insert into messages (createdAt, fk_uID) values ('" + data.createdAt + "',"+parseInt(uID)+")";
+
+//insert into messages (createdAt, fk_uID, text, fk_rmID) values ('timestamp', (select uID from users where userName = 'Zach'), 'my message is sucess.', (select rmID from rooms where roomName = 'HackReactor'));
+
+
   db.connection.query(insertMSG, function(err, rows){
     if(err){console.log(err);}
     console.log(rows);
