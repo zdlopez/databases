822daa3bd3288c129d43b4cded2ded11c83d402b
zdlopez <zachary.d.lopez@gmail.com>
2015-01-09 16:03:44 -0800

    implemented schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  uID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  userName varchar(50)
 );
+
+CREATE TABLE rooms (
+  rmID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  roomName varchar(50)
+);
+
+CREATE TABLE messages (
+  mID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt char(24),
+  fk_uID INT,
+  text varchar(200),
+  fk_rmID INT,
+  FOREIGN KEY (fk_uID)
+  REFERENCES users(uID),
+  FOREIGN KEY (fk_rmID)
+  REFERENCES rooms(rmID)
+);
+
 
 /* Create other tables and define schemas for them here! */
 
